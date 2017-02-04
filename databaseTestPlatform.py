a6a38c2c913f5d78fb1851668ed2e709892cd8f9
SeanFoley123 <sean.foley@students.olin.edu>
2017-02-04 15:17:14 -0500

    Added some more questionable functionality

---
+++ databaseTestPlatform.py
@@ -0,0 +1,18 @@
+import sqlite3
+import retrieveData as retrieve
+
+sqlite_file = "data.sqlite"
+
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+thing = retrieve.find_students_in_class(c, 1)
+print("Students in " + retrieve.find_class_name(c, 1)[0] + ": " + str(thing))
+
+thing = retrieve.find_students_classes(c, 4)
+print(thing)
+
+thing = retrieve.find_students_projects(c, 3)
+print(thing)
+
+conn.close()
--- retrieveData.py
+++ retrieveData.py
@@ -1,11 +1,40 @@
 """
 This is a module with functions that take a database and a some kind of search terms,
 and return data in some form. Basically, strip unnecessary code out of the main server section.
+We should probably condense these functions to make them more general-purpose, and maybe create a class wrapper
 """ 
 import sqlite3
 
 def find_students_in_class(c, class_id):		#Return a list of names of students in a class with a certain class_id
-	c.execute("select student_id from class_student where class_id=(?)", (class_id, ))
-	student_ids=tuple([thing[0] for thing in c.fetchall()])
+	c.execute("SELECT student_id FROM class_student WHERE class_id=(?)", (class_id, ))
+	student_ids=tuple([thing[0] for thing in c.fetchall()])		#create a tuple of all student ids in the class
 	c.execute("SELECT (name) FROM student WHERE student_id in (" + ",".join("?"*len(student_ids)) + ")", student_ids)     #This is not elegant, but I couldn't get executemany to work
 	return c.fetchall()
+
+def find_students_classes(c, student_id): 		#Return a list of the classes a student is in
+	c.execute("SELECT class_id FROM class_student WHERE student_id=(?)", (student_id, ))
+	class_ids=tuple([thing[0] for thing in c.fetchall()])		#create a tuple of all the class_ids a student has
+	c.execute("SELECT (name) FROM class WHERE class_id in (" + ",".join("?"*len(class_ids)) + ")", class_ids)     #This is not elegant, but I couldn't get executemany to work
+	return c.fetchall()
+
+def find_students_projects(c, student_id):		#Return a list of all a student's projects
+	c.execute("SELECT project_id FROM student_project WHERE student_id=(?)", (student_id, ))
+	project_ids=tuple([thing[0] for thing in c.fetchall()])
+	c.execute("SELECT title FROM project WHERE project_id in (" + ",".join("?"*len(project_ids)) + ")", project_ids)
+	return c.fetchall()
+
+def find_classes_projects(c, class_id):			#Return a list of all projects in a class
+	c.execute("SELECT title FROM project WHERE class_id=(?)", (class_id, ))
+	return c.fetchall()
+
+def find_class_name(c, class_id):				#Tell you the name of a class based on id
+	c.execute("SELECT name FROM class WHERE class_id=(?)", (class_id, ))
+	return c.fetchone()
+
+def find_student_name(c, student_id):			#Tell you the name of a student based on id
+	c.execute("SELECT name FROM student WHERE student_id=(?)", (student_id, ))
+	return c.fetchone()
+
+def find_project_title(c, project_id):			#Tell you the title of a project based on id
+	c.execute("SELECT title FROM project WHERE project_id=(?)", (project_id, ))
+	return c.fetchone()
